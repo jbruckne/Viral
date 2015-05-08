62bcf8fa84258a1088c4e826112a9214deac7fb8
Joe Bruckner <joembruckner@gmail.com>
2015-05-07 23:37:56 -0400

    Delete db_config.php

--- dbCode/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Database connection variables
-	define('DB_SERVER'  , "mydb.ics.purdue.edu");
-	define('DB_USER'    , "jbruckne");
-	define('DB_PASSWORD', "526mysql49");
-	define('DB_NAME'    , "jbrukne");
-?>
